6c1d4baf402c44565fdf05ee3a6a492b355c1f6f
Bartłomiej Buchała <Bartomiej Buchaa>
2016-04-26 18:10:37 +0200

    Improved displaying tasks in table, edition and showing details.

---
+++ WebReminder/Controllers/TaskController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebReminder.Database.DB_Operations;
+using WebReminder.Models;
+
+namespace WebReminder.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: Task
+        public ActionResult Index()
+        {
+            using (TaskDatabase db = new TaskDatabase())
+            {
+                int currentUserId;
+
+                Int32.TryParse(Session["UserId"].ToString(), out currentUserId);
+                
+                return View(db.GetUsersTasks(currentUserId));
+            }
+            
+        }
+
+        // GET: Task/Details/5
+        public ActionResult Details(int id)
+        {
+            using (TaskDatabase db = new TaskDatabase())
+            {
+                var selectedTask = db.GetTaskById(id);
+                return View(selectedTask);
+            }
+        }
+
+        // GET: Task/Create
+        public ActionResult Create()
+        {
+            return View(new Task());
+        }
+
+        // POST: Task/Create
+        [HttpPost]
+        public ActionResult Create(Task task)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View();
+
+                using (TaskDatabase db = new TaskDatabase())
+                {
+                    int currentUserId;
+
+                    Int32.TryParse(Session["UserId"].ToString(), out currentUserId);
+
+                    db.AddTaskToDatabase(task, currentUserId);
+                }
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Task/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (TaskDatabase db = new TaskDatabase())
+            {
+                var selectedTask = db.GetTaskById(id);
+                return View(selectedTask);
+            }
+        }
+
+        // POST: Task/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Task task)
+        {
+            try
+            {
+                using (TaskDatabase db = new TaskDatabase())
+                {
+                    db.ReplaceProperties(task);
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Task/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (TaskDatabase db = new TaskDatabase())
+            {
+                db.RemoveTaskById(id);
+            }
+                return RedirectToAction("Index");
+        }
+
+        //// POST: Task/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
--- WebReminder/Database/DB Operations/TaskDatabase.cs
+++ WebReminder/Database/DB Operations/TaskDatabase.cs
@@ -9,16 +9,51 @@ namespace WebReminder.Database.DB_Operations
 {
     public class TaskDatabase : ContextRepository
     {
-        public Task testFoo()
+        public Task GetTaskById(int id)
         {
-            var query = context.Tasks.FirstOrDefault();
+            var returnedTask= context.Tasks.FirstOrDefault(x => x.TaskID == id);
 
-            return query;
+            return returnedTask;
         }
 
-        public List<Task> getUsersTasks(int userId)
+        public List<Task> GetUsersTasks(int userId)
         {
             return context.Tasks.Where(x => x.Owner.UserID == userId).ToList();
+        }
+
+        public void AddTaskToDatabase(Task newTask, int ownerId)
+        {
+            var taskOwner = context.Users.FirstOrDefault(x => x.UserID == ownerId);
+
+            if (taskOwner != null)
+            {
+                newTask.Owner = taskOwner;
+
+                context.Tasks.Add(newTask);
+                SaveChanges();
+            }
+
+        }
+
+        public void RemoveTaskById(int id)
+        {
+            context.Tasks.Remove(GetTaskById(id));
+            SaveChanges();
+        }
+
+        public void ReplaceProperties(Task taskWithNewProperties)
+        {
+            var selectedTask = GetTaskById(taskWithNewProperties.TaskID);
+
+            if (selectedTask != null)
+            {
+                selectedTask.DateTime = taskWithNewProperties.DateTime;
+                selectedTask.Place = taskWithNewProperties.Place;
+                selectedTask.Name = taskWithNewProperties.Name;
+                selectedTask.Description = taskWithNewProperties.Description;
+
+                SaveChanges();
+            }
         }
     }
 
